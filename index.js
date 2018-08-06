6002cf3b69d3882e968ddadef30b3b945d3814c9
Max Petersen <mglxp5@gmail.com>
2018-08-06 13:08:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
-  var empArray = []
+  var empArray = [];
   for(var i; i < musicians.length ; i++){
     empArray[i] = "" + musicians[i] + " plays " + instruments[i]
     //console.log(empArray[i])
