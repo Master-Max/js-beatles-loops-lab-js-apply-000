35a43a66227f9163f86eaac41b1b6345b9c9e723
Max Petersen <mglxp5@gmail.com>
2018-08-06 12:52:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var empArray
+  for(var i; i < musicians.length ; i++){
+    empArray[i] = "" + musicians[i] + " plays " + instruments[i]
+    console.log(empArray[i])
+  }
+  return empArray
+}
