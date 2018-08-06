d23bc616aba7be516dc565f211c89b3aebe111b2
Max Petersen <mglxp5@gmail.com>
2018-08-06 13:13:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // add solution here
-
-
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+/*
 function theBeatlesPlay(musicians,instruments){
   var empArray = [];
   for(var i; i < musicians.length ; i++){
@@ -9,7 +15,7 @@ function theBeatlesPlay(musicians,instruments){
   }
   return empArray;
 }
-
+*/
 function johnLennonFacts(facts){
   var i = 0;
   while (i < facts.length){
