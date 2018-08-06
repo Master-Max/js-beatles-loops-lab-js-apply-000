b1c816688a86f7be55491390155f32fb4aec2747
Max Petersen <mglxp5@gmail.com>
2018-08-06 12:57:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ function theBeatlesPlay(musicians,instruments){
   var empArray
   for(var i; i < musicians.length ; i++){
     empArray[i] = "" + musicians[i] + " plays " + instruments[i]
-    console.log(empArray[i])
+    //console.log(empArray[i])
   }
-  return empArray
+  //return empArray
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  while (i < facts.length){
+    facts[i] = facts[i] + "!!!"
+  }
+}
+
+function iLoveTheBeatles(a){
+  var empArray2
+  do{
+    
+  }while()
 }
